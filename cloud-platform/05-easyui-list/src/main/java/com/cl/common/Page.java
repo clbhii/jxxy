fb305ae9abf92a2b0fa1609910dbc78bcf86ba7d
clbhii <clbhii@163.com>
2020-05-11 15:33:21 +0800

    05-easyui-list

--- cloud-platform/05-easyui-list/src/main/java/com/cl/common/Page.java
+++ cloud-platform/05-easyui-list/src/main/java/com/cl/common/Page.java
@@ -5,68 +5,76 @@ import java.io.Serializable;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * 分页
+ * @param <T>
+ */
 public class Page<T>
-        implements Serializable
-{
-    private static final long serialVersionUID = 3509375972998939764L;
-    public static String CURRENT_INDEX = "currentIndex";
-    public static String PAGE_SIZE = "pageSize";
-    public static String INDEX = "index";
+        implements Serializable {
+    /**
+     * 当前页
+     */
     private int currentIndex;
+    /**
+     * 每页条数
+     */
     private int pageSize;
+    /**
+     * 总数量
+     */
     private int totalNumber;
+    /**
+     * 总页数
+     */
     private int totalPage;
+    /**
+     * 下一页
+     */
     private int nextIndex;
+    /**
+     * 上一页
+     */
     private int preIndex;
     private List<T> Items = Collections.emptyList();
 
-    public int getPageSize()
-    {
+    public int getPageSize() {
         return this.pageSize;
     }
 
-    public Page(int totalNumber, int currentIndex, int pageSize, List<T> items)
-    {
+    public Page(int totalNumber, int currentIndex, int pageSize, List<T> items) {
         this.totalNumber = totalNumber;
         this.currentIndex = currentIndex;
         this.pageSize = pageSize;
         this.Items = items;
     }
 
-    public Page()
-    {
+    public Page() {
         this.totalNumber = 0;
         this.currentIndex = 1;
         this.pageSize = 10;
     }
 
-    public void setPageSize(int pageSize)
-    {
+    public void setPageSize(int pageSize) {
         this.pageSize = pageSize;
     }
 
-    public int getCurrentIndex()
-    {
+    public int getCurrentIndex() {
         return this.currentIndex;
     }
 
-    public void setCurrentIndex(int currentIndex)
-    {
+    public void setCurrentIndex(int currentIndex) {
         this.currentIndex = currentIndex;
     }
 
-    public int getTotalNumber()
-    {
+    public int getTotalNumber() {
         return this.totalNumber;
     }
 
-    public void setTotalNumber(int totalNumber)
-    {
+    public void setTotalNumber(int totalNumber) {
         this.totalNumber = totalNumber;
     }
 
-    public int getTotalPage()
-    {
+    public int getTotalPage() {
         int size = this.totalNumber / this.pageSize;
         if (this.totalNumber % this.pageSize != 0) {
             size += 1;
@@ -76,8 +84,7 @@ public class Page<T>
         return this.totalPage;
     }
 
-    public int getNextIndex()
-    {
+    public int getNextIndex() {
         if (this.currentIndex >= getTotalPage()) {
             this.nextIndex = this.currentIndex;
         } else {
@@ -86,8 +93,7 @@ public class Page<T>
         return this.nextIndex;
     }
 
-    public int getPreIndex()
-    {
+    public int getPreIndex() {
         if (this.currentIndex <= 1) {
             this.preIndex = 0;
         } else {
@@ -96,19 +102,21 @@ public class Page<T>
         return this.preIndex;
     }
 
-    public List<T> getItems()
-    {
+    public List<T> getItems() {
         return this.Items;
     }
 
-    public void setItems(List<T> items)
-    {
+    public void setItems(List<T> items) {
         this.Items = items;
     }
 
-   public int getStart() {
-       return getPreIndex() * this.pageSize;
-   }
+    /**
+     * 偏移量，开始位置
+     * @return
+     */
+    public int getStart() {
+        return getPreIndex() * this.pageSize;
+    }
 
 
 }
--- cloud-platform/05-easyui-list/src/main/webapp/WEB-INF/jsp/index.jsp
+++ cloud-platform/05-easyui-list/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -2,11 +2,10 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Full Layout - jQuery EasyUI Demo</title>
+	<title>首页</title>
 	<link rel="stylesheet" type="text/css" href="../css/themes/default/easyui.css">
 	<link rel="stylesheet" type="text/css" href="../css/themes/icon.css">
 	<link rel="stylesheet" type="text/css" href="../css/demo.css">
-<!--	<link rel="stylesheet" type="text/css" href="sidemenu_style.css">-->
 	<script type="text/javascript" src="../js/jquery.min.js"></script>
 	<script type="text/javascript" src="../js/jquery.easyui.min.js"></script>
 
@@ -21,7 +20,6 @@
 		},{
 			text: '终端管理',
 			iconCls: 'fa fa-at',
-			selected: true,
 			children: [{
 				text: '设备管理',
 				url:'../mvc/devicelist'
@@ -34,7 +32,6 @@
 					title: item.text,
 					content: '<iframe scrolling="auto" frameborder="0"  src="' + item.url + '" style="width:100%;height:99%;"></iframe>',
 					closable: true,
-					icon: item.iconCls,
 					id: item.id
 				});
 			} else {
@@ -45,13 +42,12 @@
 </head>
 <body class="easyui-layout">
 	<div data-options="region:'north',border:false" style="height:60px;background:#B3DFDA;padding:10px"><h1 >物联网云平台</h1></div>
-	<div data-options="region:'west',split:true,title:'菜单'" style="width:250px;padding:10px;">
-		<div id="leftMenu" class="easyui-sidemenu" data-options="data:data,onSelect: onSideMenuSelect" ></div>
-	</div>
-	<div data-options="region:'east',split:true,collapsed:true,title:'East'" style="width:100px;padding:10px;">east region</div>
-	<div data-options="region:'center',title:'Center'">
-
-		<div id="mainTab" class="easyui-tabs" data-options="tools:'#tab-tools'" style="width:100%;height:99%">
-	</div>
+		<div data-options="region:'west',split:true,title:'菜单'" style="width:250px;padding:10px;">
+			<div id="leftMenu" class="easyui-sidemenu" data-options="data:data,onSelect: onSideMenuSelect" ></div>
+		</div>
+		<div data-options="region:'center'">
+			<div id="mainTab" class="easyui-tabs" data-options="" style="width:100%;height:99%">
+			</div>
+		</div>
 </body>
 </html>
